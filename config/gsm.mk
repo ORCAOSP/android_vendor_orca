0030d2d8d8960b9fcbb9ad6defd94a4bfee779cb
Orca Project <shamukillaz@orcaproject.co>
2013-08-06 23:29:45 -0400

    Orca 3 WIP Commit: This is Android 4.3

---
+++ config/gsm.mk
@@ -0,0 +1,11 @@
+# GSM APN list
+PRODUCT_COPY_FILES += \
+    vendor/orca/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    gsm.current.phone-type=1
+
+# Packages
+PRODUCT_PACKAGES += \
+    Stk \
+    CellBroadcastReceiver
